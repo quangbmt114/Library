70d07b537c905e1206951bee8c86d6f82b5840d7
quangbmt114 <81748288+quangbmt114@users.noreply.github.com>
2024-01-27 21:56:38 +0700

    config source

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { Button } from "@material-tailwind/react";
+export default function Home() {
+  return (
+    <>
+      <Button placeholder={undefined}>Button</Button>
+    </>
+  );
+}
